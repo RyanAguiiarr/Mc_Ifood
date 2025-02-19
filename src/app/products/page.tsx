fd6a3f846b95e2814f368e89deb488b11493ef00
Ryan Cantareli de Aguiar <165313458+RyanAguiiarr@users.noreply.github.com>
2025-02-19 11:18:21 -0300

    finalizando back-end e configurando twind e chadcn

---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+const ProductPage = () => {
+  return (
+    <>
+      <div className="p-5 border border-red-500 rounded-xl">
+        <h1 className="text-red-500">products page</h1>
+        <Button>teste</Button>
+      </div>
+    </>
+  );
+};
+
+export default ProductPage;
